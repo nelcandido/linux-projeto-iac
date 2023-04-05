0eb4c6858a350ed2044ffc38564cc4edd2ae6212
nelcandido <nelcandido@gmail.com>
2023-04-04 23:43:25 -0300

    Arquivo IaC v.1

---
+++ iac1.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+echo "Script para provisionamento de infraestrutura"
+
+echo "Iniciando script..."
+
+echo "Criando diretórios..."
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo "Criando grupos de usuários..."
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando usuários..."
+
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123)
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123)
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123)
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123)
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123)
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123)
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123)
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123)
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123)
+
+echo "Adicionando usuários aos grupos..."
+
+usermod -g GRP_ADM carlos
+usermod -g GRP_ADM maria
+usermod -g GRP_ADM joao
+usermod -g GRP_VEN debora
+usermod -g GRP_VEN sebastiana
+usermod -g GRP_VEN roberto
+usermod -g GRP_SEC josefina
+usermod -g GRP_SEC amanda
+usermod -g GRP_SEC rogerio
+
+echo "Configurando permissões de diretórios..."
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+echo "Script finalizado!"
